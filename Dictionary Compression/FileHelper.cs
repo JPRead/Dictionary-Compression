b1ff52db9bf2701dc2f869949e16be683aa9e2bd
JPRead <jpread@student.palmers.ac.uk>
2017-11-24 18:00:22 +0000

    First Sync
    Currently creates a dictionary given a file. Will need to write an
    output to a seperate file.

---
+++ Dictionary Compression/FileHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Dictionary_Compression
+{
+    class FileHelper
+    {
+        public FileHelper()
+        {
+        }
+
+        public string Open(string directory)
+        {
+            StreamReader s = new StreamReader(directory);
+            string str = "";
+            while (!s.EndOfStream)
+            {
+                str += (char)s.Read();
+            }
+            return str;
+        }
+
+        public string[] Split(string str)
+        {
+            //Put within [] all the characters to split at
+            return Regex.Split(str, @"([?<= .,'!?])");
+        }
+
+        public Dictionary<int, string> CreateDictionary(string[] strArray)
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+            int index = 1;
+            dict.Add(0, strArray[0]);
+            for (int i = 1; i < strArray.Length; i++)
+            {
+                if (!dict.ContainsValue(strArray[i]))
+                {
+                    dict.Add(index, strArray[i]);
+                    index++;
+                }
+            }
+
+            return dict;
+        }
+    }
+}
---
+++ Dictionary Compression/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_Compression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileHelper f = new FileHelper();
+
+            //Get directory of executable
+            string currentDirectory = System.IO.Path.GetDirectoryName(System.Diagnostics.Process.G‌​etCurrentProcess().M‌​ainModule.FileName);
+
+            //Look back to project directory, Double up each backslash, and add the directory for the text file
+            char[] CurDirArray = currentDirectory.ToCharArray();
+            currentDirectory = "";
+            for(int i = 0; i <= CurDirArray.Length - 10; i++)
+            {
+                currentDirectory += CurDirArray[i];
+                if(CurDirArray[i] == Convert.ToChar("\\"))
+                {
+                    currentDirectory += "\\";
+                }
+            }
+            currentDirectory += "files\\input.txt";
+
+            //Open and split the file into an array
+            string str = f.Open(currentDirectory);
+            string[] strArray = f.Split(str);
+
+            for( int i = 0; i < strArray.Length; i++)
+            {
+                Console.Write(strArray[i] + "|");
+            }
+            
+            //Create dictionary from the array
+            Dictionary<int, string> dict =  f.CreateDictionary(strArray);
+
+            for (int i = 0; i < dict.Count; i++)
+            {
+                Console.WriteLine(dict.ElementAt(i));
+            }
+
+            Console.Read();
+        }
+    }
+}
